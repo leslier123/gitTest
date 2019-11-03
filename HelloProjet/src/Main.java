554793f1a7ec3fd237095c7ecad435376da621cc
renxufeng <renxufeng2917@163.com>
2019-11-03 12:51:28 +0800

    seconde commit

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
+		System.out.println("this is  test code from local codes secondeTimes");
 		System.out.println("this is  test code from local codes");
         System.out.println("this is  test code");
         System.out.println("HelloWorld!");
